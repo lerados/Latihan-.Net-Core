9bfa8fef41535e5111a40cab6b7df161ec544c38
Galih Satria Permana <43876061+lerados@users.noreply.github.com>
2019-11-06 21:18:17 +0700

    Pengulangan For, While dan Do While

---
+++ Pengulangan/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Pengulangan_dan_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int w1 = 10;
+            int w2;
+            int dw;
+
+            Console.WriteLine("Pengulangan For Membentuk Segitiga");
+            for (int i = 0; i < 10; i++)
+            {
+                for (int x = 0; x < i; x++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("*");
+            }
+            Console.ReadLine();
+
+            Console.WriteLine("Pengulangan While Membentuk Segitiga");
+            while (w1 > 0)
+            {
+                w2 = w1 - 1;
+                while (w2 > 0)
+                {
+                    Console.Write("*");
+                    w2--;
+                }
+                Console.WriteLine("*");
+                w1--;
+            }
+            Console.ReadLine();
+
+            Console.WriteLine("Pengulangan Do While");
+            Console.WriteLine("Masukan angka 0 untuk mengakhiri pengulangan");
+            do
+            {
+                dw = int.Parse(Console.ReadLine());
+            }
+            while (dw != 0);
+            Console.WriteLine("Pengulangan berhenti");
+            Console.ReadLine();
+        }
+    }
+}
